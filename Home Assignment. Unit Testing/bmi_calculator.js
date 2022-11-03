93e974a234af9303eef1388b36c4753f21d10db0
ericssontee <ericssontee.eb@gmail.com>
2022-11-03 19:03:26 +0800

    Revert "Automated Testing for BMI Calculator."
    This reverts commit c8bcee4b04e591bcd4a5213ed86f3bd6040b55a3.

--- Home Assignment. Unit Testing/bmi_calculator.js
+++
@@ -1,9 +0,0 @@
-const bmiCalculator = (kg, m) => {
-    const bmi = kg/(m ** 2)
-
-    return +bmi.toFixed(2); // This is updated after multiple testing with JEST as toFixed returns the number value as string.
-                            // To fix this, added + before bmi.toFixed(2). It may not be the best practice as it will conflict with other syntax affects performance but for now it gets the job done.
-}
-
-module.exports = bmiCalculator;
-
--- Home Assignment. Unit Testing/bmi_calculator.test.js
+++
@@ -1,5 +0,0 @@
-const bmiCalc = require('./bmi_calculator');
-
-test("Check if bmiCalculator correctly calculates", () => {
-    expect(bmiCalc(95,1.70)).toBe(32.87);
-});
