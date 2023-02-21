17c3c24de26507fe0e97ed5ace71749e13200752
ericssontee <ericssontee.eb@gmail.com>
2023-02-21 22:52:53 +0800

    Individual Project Commit

---
+++ JS/Individual Project. Online Clothing Store/script.js
@@ -0,0 +1,25 @@
+const shopInformation = [
+  {
+    store: "Zara",
+    location: "Manhattan Avenue, New York",
+    code: "NY656",
+  },
+];
+
+const userInformation = [
+    { 
+        firstName: 'Angelina',
+        lastName: 'Jolie',
+        age: 47,
+        birthday: '4.06.1975' 
+    }
+];
+
+const items = [
+    {
+        iteam_name: 'Blue Jeans',
+        category: 'Trousers',
+        price: 50,
+        stock_qty: 456
+    }
+]
