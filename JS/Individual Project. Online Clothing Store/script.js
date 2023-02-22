9267c8b7e1d770c44d7bfc76ea87680ebf4824a5
ericssontee <ericssontee.eb@gmail.com>
2023-02-22 23:11:24 +0800

    IP update commit

--- JS/Individual Project. Online Clothing Store/script.js
+++ JS/Individual Project. Online Clothing Store/script.js
@@ -17,7 +17,13 @@ const userInformation = [
 
 const items = [
     {
-        iteam_name: 'Blue Jeans',
+        item_name: 'Blue Jeans',
+        category: 'Trousers',
+        price: 50,
+        stock_qty: 456
+    },
+    {
+        item_name: 'Red Jeans',
         category: 'Trousers',
         price: 50,
         stock_qty: 456
@@ -37,3 +43,31 @@ function addUsers (newFirstName, newLastName, newAge, dayBirth, monthBirth, year
 // addUsers('Eric', 'Tee', 27, 11, 9, 1996);
 
 // console.log(userInformation);
+
+
+function addUpdateItem (itemName, itemCategory, itemPrice, itemQty) {
+    const addItemObj = {
+        item_name: itemName,
+        category: itemCategory,
+        price: itemPrice,
+        stock_qty: itemQty
+    }
+
+    const indexItem = items.findIndex(item => item.item_name === addItemObj.item_name && item.category === addItemObj.category)
+
+    if (indexItem > 0) {
+        // items[indexItem].price = addItemObj.price
+        items[indexItem].stock_qty = addItemObj.stock_qty
+        console.log(`Stocks of ${items[indexItem].item_name} has been updated`);
+        console.log(items[indexItem]);
+    } else {
+        items.push(addItemObj)
+        console.log(`Item ${items[items.length - 1].item_name} has been Added`);
+        console.log(items[items.length - 1])
+    }
+
+}
+
+addUpdateItem('Red JeansS', 'Trousers', 60, 400);
+
+addUpdateItem('Red JeansS', 'Trousers', 60, 450);
