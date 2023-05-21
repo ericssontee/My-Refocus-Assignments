476bc23e8b3cba6113867206f6a965e53603f87c
ericssontee <ericssontee.eb@gmail.com>
2023-05-21 19:50:43 +0800

    HA React commit

---
+++ ReactJS/Home Assignment. Functional Components/ha-function-component/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Input from './components/Input';
+
+function App() {
+  return (
+    <div className="App">
+      Home Assignment. Functional Components
+      <Input />
+    </div>
+  );
+}
+
+export default App;
